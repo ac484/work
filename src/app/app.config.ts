56889b5d0b8281bec1b8478c457611d5cae32439
𓂺 <ac7x7x@gmail.com>
2025-07-12 06:17:26 +0800

    first commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,36 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import Aura from '@primeuix/themes/aura';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai';
+import { environment } from '../environments/environment';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: { darkModeSelector: '.p-dark' },
+      },
+    }),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "lin-in", appId: "1:387803341154:web:a8e5216dfb18b4d7a78c3b", storageBucket: "lin-in.firebasestorage.app", apiKey: "AIzaSyCX4rENtBHJAxypxNpx5YrFU-gHZl3L2-s", authDomain: "lin-in.firebaseapp.com", messagingSenderId: "387803341154", measurementId: "G-20Y88DSFFS" })), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideAppCheck(() => {
+  // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+  const provider = new ReCaptchaEnterpriseProvider(environment.recaptchaSiteKey);
+  return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+}), provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), providePerformance(() => getPerformance()), provideStorage(() => getStorage()), provideRemoteConfig(() => getRemoteConfig()), provideVertexAI(() => getVertexAI()),
+  ],
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCX4rENtBHJAxypxNpx5YrFU-gHZl3L2-s",
+    authDomain: "lin-in.firebaseapp.com",
+    projectId: "lin-in",
+    storageBucket: "lin-in.firebasestorage.app",
+    messagingSenderId: "387803341154",
+    appId: "1:387803341154:web:a8e5216dfb18b4d7a78c3b",
+    measurementId: "G-20Y88DSFFS"
+  },
+  recaptchaSiteKey: '6LfZgXwrAAAAABMJ0e0Ym-ZbBuoJU5AFdXJa90am' // 從 Google Cloud Console 獲取
+};
