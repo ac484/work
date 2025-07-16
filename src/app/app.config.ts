af79bc454a62d507443e0bfcbe886136ad34b999
ac484 <ac7x7x@gmail.com>
2025-07-16 15:53:44 +0800

    chore: initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -32,6 +32,26 @@ import { environment } from '@env/environment';
 import { formlyConfigFactory, PaginatorI18nService } from '@shared';
 import { InMemDataService } from '@shared/in-mem/in-mem-data.service';
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import {
+  initializeAppCheck,
+  ReCaptchaEnterpriseProvider,
+  provideAppCheck,
+} from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
+import { getVertexAI, provideVertexAI } from '@angular/fire/vertexai';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -114,5 +134,32 @@ export const appConfig: ApplicationConfig = {
         popupHeaderDateLabel: 'MMM dd, E',
       },
     }),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'lin-in',
+        appId: '1:387803341154:web:a8e5216dfb18b4d7a78c3b',
+        storageBucket: 'lin-in.firebasestorage.app',
+        apiKey: 'AIzaSyCX4rENtBHJAxypxNpx5YrFU-gHZl3L2-s',
+        authDomain: 'lin-in.firebaseapp.com',
+        messagingSenderId: '387803341154',
+        measurementId: 'G-20Y88DSFFS',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideAppCheck(() => {
+      // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+      const provider = new ReCaptchaEnterpriseProvider('6LfZgXwrAAAAABMJ0e0Ym-ZbBuoJU5AFdXJa90am');
+      return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+    }),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideStorage(() => getStorage()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideVertexAI(() => getVertexAI()),
   ],
 };
